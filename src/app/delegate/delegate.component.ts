c8a04760f7ff304df103a1afe3b3a7ac1c2341ab
John Piesco <jp@on-core.com>
2018-04-03 16:53:25 -0400

    ngfor canvas triangles

--- src/app/delegate/delegate.component.ts
+++ src/app/delegate/delegate.component.ts
@@ -17,6 +17,7 @@ export class DelegateComponent implements OnInit {
       this.myjson = JSON.parse(data["_body"]);
       let myeval = eval(this.myjson['fn'][0]);
       this.processjson(myeval);
+      this.createCanvas();
     });
 
   }
@@ -31,6 +32,32 @@ export class DelegateComponent implements OnInit {
   }
   mout(ev){
     document.getElementById(ev).style.fill = 'lime';
+  }
+  canvasarray = [{id: "canvas0", width: "300", height: "300", style:""}];
+  canvasadder = 0;
+  createCanvas(){
+    this.canvasarray.forEach(element => {
+      let mycanvas = <HTMLCanvasElement>document.getElementById(element.id);
+      if(mycanvas.getContext('2d')){
+        // let canvas: HTMLCanvasElement;
+        // let mycanvasobj = {id: "canvas" + this.canvasadder};
+        let ctx: CanvasRenderingContext2D;
+        ctx = mycanvas.getContext('2d');
+         // Stroked triangle
+         ctx.beginPath();
+         ctx.moveTo(125, 125);
+         ctx.lineTo(125, 45);
+         ctx.lineTo(45, 125);
+         ctx.closePath();
+         ctx.stroke();
+      }
+      else{
+        this.createCanvas();
+      }
+    });
+    
+
+     //this.canvasadder++;
   }
 // If final connection comes from above then adding all points to (left) of border
 // If final connection comes from below than add all points to right of border
