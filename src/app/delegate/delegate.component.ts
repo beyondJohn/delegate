1eb6bd15012303358c5c16ce5781e39a4a8691ec
John Piesco <jp@on-core.com>
2018-04-04 18:59:55 -0400

    add callback to http call

--- src/app/delegate/delegate.component.ts
+++ src/app/delegate/delegate.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+//import 'rxjs/RX';
 
 @Component({
   selector: 'app-delegate',
@@ -11,59 +13,63 @@ export class DelegateComponent implements OnInit {
   constructor(
     private _http: Http
   ) { }
-
+  canvasadder = 0;
+  breaker;
   ngOnInit() {
-    this._http.get("../../assets/myjson.json").subscribe((data) => {
-      this.myjson = JSON.parse(data["_body"]);
-      let myeval = eval(this.myjson['fn'][0]);
-      this.processjson(myeval);
-      this.createCanvas();
-    });
-
+    this.getJson().subscribe(json => this.processJson(json), json => this.error(json), () => this.complete());
   }
   myjson;
-  processjson(fn){
-    fn();
+  getJson() {
+    return this._http.get("../../assets/myjson.json").map(response => response.json()); //map response.json() converts response to JSON object
   }
-  mover(ev){
+  executejsonfn(fn) {
+    this.canvasadder = fn(this.canvasadder);
+    this.breaker = "hi";
+  }
+  processJson(jsondata) {
+    this.myjson = jsondata;
+    if (this.myjson['fn'] !== undefined) {
+      let myeval = eval(this.myjson['fn'][0]);
+      this.executejsonfn(myeval);
+      let checkadder = this.canvasadder;
+    }
+  }
+  mover(ev) {
     document.getElementById(ev).style.fill = 'blue';
     document.getElementById(ev).style.cursor = 'pointer';
     console.log(ev);
   }
-  mout(ev){
+  mout(ev) {
     document.getElementById(ev).style.fill = 'lime';
   }
-  canvasarray = [{id: "canvas0", width: "300", height: "300", style:""},{id: "canvas0", width: "300", height: "300", style:""}];
-  canvasadder = 0;
-  createCanvas(){
+  canvasarray = [{ id: "canvas0", width: "300", height: "300", style: "" }, { id: "canvas0", width: "300", height: "300", style: "" }];
+
+  createCanvas() {
     this.canvasarray.forEach(element => {
       let mycanvas = <HTMLCanvasElement>document.getElementById(element.id);
-      if(mycanvas.getContext('2d')){
+      if (mycanvas.getContext('2d')) {
         // let canvas: HTMLCanvasElement;
-        // let mycanvasobj = {id: "canvas" + this.canvasadder};
+        let mycanvasobj = { id: "canvas" + this.canvasadder };
         let ctx: CanvasRenderingContext2D;
         ctx = mycanvas.getContext('2d');
-         // Stroked triangle
-         ctx.beginPath();
-         ctx.moveTo(125, 125);
-         ctx.lineTo(125, 45);
-         ctx.lineTo(45, 125);
-         ctx.closePath();
-         ctx.stroke();
+        // Stroked triangle
+        ctx.beginPath();
+        ctx.moveTo(125, 125);
+        ctx.lineTo(125, 45);
+        ctx.lineTo(45, 125);
+        ctx.closePath();
+        ctx.stroke();
       }
-      else{
+      else {
         this.createCanvas();
       }
     });
-    
+  }
+  clicked() {
+    console.log('clicked , canvasadder: ', this.canvasadder);
+  }
+  bodyclick(ev, id) {
 
-     //this.canvasadder++;
-  }
-  clicked(){
-    console.log('clicked');
-  }
-  bodyclick(ev, id){
-    
     let topclick, leftclick, parentwidth, parentheight, toppercent, leftpercent, scaledwidth, found, top, left;
     topclick = ev.layerY;
     leftclick = ev.layerX;
@@ -73,10 +79,17 @@ export class DelegateComponent implements OnInit {
     leftpercent = parseFloat(((parseInt(leftclick) * 100) / parseInt(parentwidth)).toString()).toFixed(0);
     toppercent = parseFloat(((parseInt(topclick) * 100) / parseInt(parentheight)).toString()).toFixed(0);
   }
-// If final connection comes from above then adding all points to (left) of border
-// If final connection comes from below than add all points to right of border
+  error(json) {
+    console.log(json);
+  }
+  complete() {
+    this.createCanvas();
+    console.log("fin");
+  }
+  // If final connection comes from above then adding all points to (left) of border
+  // If final connection comes from below than add all points to right of border
 
-// Have to track each point for left and right turns to determine direction of calculation 
-  
+  // Have to track each point for left and right turns to determine direction of calculation 
+
   //https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 }
