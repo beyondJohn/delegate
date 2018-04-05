7644342a763541825327aa9c8cbf57ad84888e6e
John Piesco <jp@on-core.com>
2018-04-04 20:11:01 -0400

    svg atts moved to json

--- src/app/delegate/delegate.component.ts
+++ src/app/delegate/delegate.component.ts
@@ -13,27 +13,38 @@ export class DelegateComponent implements OnInit {
   constructor(
     private _http: Http
   ) { }
-  canvasadder = 0;
-  breaker;
-  ngOnInit() {
-    this.getJson().subscribe(json => this.processJson(json), json => this.error(json), () => this.complete());
-  }
+  adder = 0;
   myjson;
+  svgs = [];
+  ngOnInit() {
+    this.getJson().subscribe(json => {this.myjson = json; this.processJson(json)}, er => this.error(er), () => this.complete());
+  }
   getJson() {
     return this._http.get("../../assets/myjson.json").map(response => response.json()); //map response.json() converts response to JSON object
   }
-  executejsonfn(fn) {
-    this.canvasadder = fn(this.canvasadder);
-    this.breaker = "hi";
-  }
   processJson(jsondata) {
-    this.myjson = jsondata;
-    if (this.myjson['fn'] !== undefined) {
-      let myeval = eval(this.myjson['fn'][0]);
+    if(jsondata['svg'].length > 0){
+      jsondata['svg'].forEach(svgobj => {
+        this.svgs.push(svgobj);
+      });
+    }
+    if (jsondata['fn'] !== undefined) {
+      let myeval = eval(jsondata['fn'][0]);
       this.executejsonfn(myeval);
-      let checkadder = this.canvasadder;
+      let checkadder = this.adder;
     }
   }
+  executejsonfn(fn) {
+    this.adder = fn(this.adder);
+  }
+  error(json) {
+    console.log("error: ", json);
+  }
+  complete() {
+    console.log("fin");
+  }
+
+  // Page Events
   mover(ev) {
     document.getElementById(ev).style.fill = 'blue';
     document.getElementById(ev).style.cursor = 'pointer';
@@ -42,32 +53,7 @@ export class DelegateComponent implements OnInit {
   mout(ev) {
     document.getElementById(ev).style.fill = 'lime';
   }
-  canvasarray = [{ id: "canvas0", width: "300", height: "300", style: "" }, { id: "canvas0", width: "300", height: "300", style: "" }];
-
-  createCanvas() {
-    this.canvasarray.forEach(element => {
-      let mycanvas = <HTMLCanvasElement>document.getElementById(element.id);
-      if (mycanvas.getContext('2d')) {
-        // let canvas: HTMLCanvasElement;
-        let mycanvasobj = { id: "canvas" + this.canvasadder };
-        let ctx: CanvasRenderingContext2D;
-        ctx = mycanvas.getContext('2d');
-        // Stroked triangle
-        ctx.beginPath();
-        ctx.moveTo(125, 125);
-        ctx.lineTo(125, 45);
-        ctx.lineTo(45, 125);
-        ctx.closePath();
-        ctx.stroke();
-      }
-      else {
-        this.createCanvas();
-      }
-    });
-  }
-  clicked() {
-    console.log('clicked , canvasadder: ', this.canvasadder);
-  }
+  // (click)="bodyclick($event,'divbody')"
   bodyclick(ev, id) {
 
     let topclick, leftclick, parentwidth, parentheight, toppercent, leftpercent, scaledwidth, found, top, left;
@@ -79,17 +65,34 @@ export class DelegateComponent implements OnInit {
     leftpercent = parseFloat(((parseInt(leftclick) * 100) / parseInt(parentwidth)).toString()).toFixed(0);
     toppercent = parseFloat(((parseInt(topclick) * 100) / parseInt(parentheight)).toString()).toFixed(0);
   }
-  error(json) {
-    console.log(json);
-  }
-  complete() {
-    this.createCanvas();
-    console.log("fin");
-  }
-  // If final connection comes from above then adding all points to (left) of border
-  // If final connection comes from below than add all points to right of border
 
-  // Have to track each point for left and right turns to determine direction of calculation 
-
-  //https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+  //create canvas (not used intended for production, decided to use svg for production)
+  // <div *ngFor="let canv of canvasarray">
+  //     <canvas id="{{canv.id}}" width="{{canv.width}}" height="{{canv.height}}" (click)="clicked()"></canvas>
+  // </div>
+  // canvasarray = [{ id: "canvas0", width: "300", height: "300", style: "" }, { id: "canvas0", width: "300", height: "300", style: "" }];
+  // createCanvas() {
+  //   this.canvasarray.forEach(element => {
+  //     let mycanvas = <HTMLCanvasElement>document.getElementById(element.id);
+  //     if (mycanvas.getContext('2d')) {
+  //       // let canvas: HTMLCanvasElement;
+  //       let mycanvasobj = { id: "canvas" + this.canvasadder };
+  //       let ctx: CanvasRenderingContext2D;
+  //       ctx = mycanvas.getContext('2d');
+  //       // Stroked triangle
+  //       ctx.beginPath();
+  //       ctx.moveTo(125, 125);
+  //       ctx.lineTo(125, 45);
+  //       ctx.lineTo(45, 125);
+  //       ctx.closePath();
+  //       ctx.stroke();
+  //     }
+  //     else {
+  //       this.createCanvas();
+  //     }
+  //   });
+  // }
+  // clicked() {
+  //   console.log('clicked , canvasadder: ', this.canvasadder);
+  // }
 }
